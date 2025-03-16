288a9f7a4b2bc8ab20349051d82c163fbdeecce9
Max Kroslak <max.kroslak@gmai;.com>
2025-03-16 21:36:32 +0100

    Starting frontend

--- my-app-fe/src/App.tsx
+++ my-app-fe/src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, FormEvent } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+function Header() {
+  return (
+    <nav>
+      <div id="userDiv">
+        <button type="submit" > Zaregistrovat sa </button>
+        <button type="submit" > Prihlasit sa </button>
+      </div>
+    </nav>
+  );
+}
+
+function FindByName() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(`Hľadaný výraz: ${searchTerm}`);
+  };
 
   return (
+    <div>
+      <form onSubmit={handleSearch} className="flex gap-2">
+        <input
+          type="text"
+          placeholder="Zadajte reštauráciu..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit" > Search </button>
+      </form>
+    </div>
+  );
+}
+
+function App({ children }: any) {
+  return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header></Header>
+      <FindByName></FindByName>
+      <h1>Vitajte na hlavnej stránke!</h1>
+      <p> Stranka bude sluzit na vyhladavanie a recenzovanie restauracii. Na pisanie recenzii je sa nutne prihlasit. </p>
+      <div>{children}</div>
     </>
-  )
+  );
 }
 
 export default App
