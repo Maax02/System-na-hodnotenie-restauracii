beb3425debe767df6976977f2844b92d634c7f41
Max Kroslak <max.kroslak@gmai;.com>
2025-04-13 01:59:53 +0200

    routes for restaurant queries

---
+++ my-app-be/routes/api_v1/restaurants.js
@@ -0,0 +1,32 @@
+var express = require('express'); // ESM: import
+var { getRestaurants, getRestaurantById } = require('../../models/restaurants')
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    getRestaurants().then(
+        (restaurant) => {
+            res.json(restaurant.rows);
+        }
+    ).catch(
+        (err) => {
+            console.log(err);
+            res.status(500);
+        }
+    );
+});
+
+router.get('/:id', function (req, res, next) {
+    const id = req.params.id;
+    getRestaurantById(id).then(
+        (restaurant) => {
+            res.json(restaurant.rows);
+        }
+    ).catch(
+        (err) => {
+            console.log(err);
+            res.status(500);
+        }
+    );
+});
+
+module.exports = router; // ESM: export
