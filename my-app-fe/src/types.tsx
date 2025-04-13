c4640ac7cf8f6ac816f438e180be8d9175c1b879
Max Kroslak <max.kroslak@gmai;.com>
2025-04-13 02:02:29 +0200

    exporting restaurant types

---
+++ my-app-fe/src/types.tsx
@@ -0,0 +1,9 @@
+export interface Restaurant {
+    restaurant_id: number;
+    restaurant_name: string;
+    street: string;
+    street_number: number;
+    city: string;
+    psc: number;
+    kuchyna: string;
+  }
