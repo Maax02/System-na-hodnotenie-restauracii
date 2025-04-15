4b1281a52c62ac71e03648897e1e7cfa1a57c96f
Max Kroslak <max.kroslak@gmai;.com>
2025-04-15 17:53:13 +0200

    adding reviews to restaurant page

---
+++ my-app-fe/src/components/restaurantPageCard.tsx
@@ -0,0 +1,49 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Restaurant } from '../types'; // extend your types if needed
+import { getRestaurantById } from '../services/restaurantService'
+import '/src/css/restaurantPage.css'
+
+
+function RestaurantPageCard() {
+    const { id } = useParams<{ id: string }>();
+    const restaurantId = id?.split('_')[0];
+    const [restaurant, setRestaurant] = useState<Restaurant[]>();
+    const [loading, setLoading] = useState(true);
+
+  // periodically refresh (timer)
+    if(restaurantId) {
+        useEffect(() => {
+        getRestaurantById(restaurantId).then(
+            (restaurant) => setRestaurant(restaurant)
+        );
+        setLoading(false);
+
+        const fetchMessagesInterval = setInterval(() => {
+            getRestaurantById(restaurantId).then(
+                (restaurant) => setRestaurant(restaurant)
+            );
+            setLoading(false);
+            }, 10000);
+        return () => clearInterval(fetchMessagesInterval);
+        }, [restaurantId]);
+    }
+
+    if (loading) return <p> Loading ... </p>;
+    if (!restaurant) return <p>Restaurant not found.</p>;
+
+    console.log(`src/images/${restaurant[0].restaurant_id}.png`)
+    return (
+    <div className='restaurant-page-card'>
+        <img src={`/images/${restaurant[0].restaurant_id}.png`} alt="rest-0" className="restaurant-page-image" />
+        <h1 className='restaurant-page-name'> {restaurant[0].restaurant_name} </h1>
+        <p className='restaurant-page-kitchen'> Kuchyna: {restaurant[0].kuchyna} </p>
+        <p className='restaurant-page-rating'> Rating: ⭐ treba dorobit /10 </p>
+        <p className='restaurant-page-address'> Adresa: {restaurant[0].street} {restaurant[0].street_number}, {restaurant[0].city}, {restaurant[0].psc} </p>
+        
+    </div>
+
+    );
+}
+
+export default RestaurantPageCard;
