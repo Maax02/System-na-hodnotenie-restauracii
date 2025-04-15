ded71cbcc7213cd022f060ac5f06fe14e328e1a6
Max Kroslak <max.kroslak@gmai;.com>
2025-04-15 17:48:23 +0200

    adding account page to navbar as link

--- my-app-fe/src/components/navBar.tsx
+++ my-app-fe/src/components/navBar.tsx
@@ -4,6 +4,7 @@ function NavBar() {
     return <nav className="navigation">
         <a href="/" className="title"> Systém na hodnotenie reštaurácií </a>
         <ul>
+            <li> <a href="/account"> Account </a> </li>
             <li> <a href="/search"> Search </a> </li>
             <li> <a href="/logIn"> Log in </a> </li>
             <li> <a href="/signUp"> Sign up </a> </li>
