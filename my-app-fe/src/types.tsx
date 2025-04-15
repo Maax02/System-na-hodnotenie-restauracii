f59c00883280714e4e48aab4b079d34ebe9f372d
Max Kroslak <max.kroslak@gmai;.com>
2025-04-15 17:47:51 +0200

    exporting review type

--- my-app-fe/src/types.tsx
+++ my-app-fe/src/types.tsx
@@ -15,3 +15,18 @@ export interface Restaurant {
     user_email: string;
     isadmin: boolean;
   }
+
+export interface RestaurantReview {
+  recenzia_id: number;
+  user_id: number;
+  restaurant_id: number;
+  hodnotenie: number;
+  sprava: string;
+  datum: Date;
+  user_name: string;
+}
+
+export interface RestaurantReviewResponse {
+  reviews: RestaurantReview[];
+  average_rating: number | null;
+}
