5da4ae4f739f9e808a24faa6de09fb7a7f8e9260
Max Kroslak <max.kroslak@gmai;.com>
2025-04-15 17:54:01 +0200

    fetching reviws

---
+++ my-app-fe/src/services/reviewService.tsx
@@ -0,0 +1,18 @@
+function getRestaurantReviews(id: string) {
+    return fetch(`/api/v1/reviews/${id}`).then(  // promise is resolved
+        (response) => {
+            if (!response.ok) { // HTTP status code NOT between 200-299
+                throw new Error("Error getting reviews");
+            }
+            return response.json();
+        }).catch((error) => {               // promise is rejected  
+            // Better way would be to throw error here and let the 
+            // client handle (e.g. show error message)
+            // Returning empty array for simplicity only!
+            console.log("Error getting reviews", error);
+            return [];
+        });
+}
+
+
+export { getRestaurantReviews };
