aaf52da3c83950ea809752fdc556ec0c64152b3e
Max Kroslak <max.kroslak@gmai;.com>
2025-04-19 18:06:41 +0200

    utils for authentication

---
+++ my-app-be/utils/authHelpers.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
+// returns promise!
+const hashPassword = (password) => {
+    return bcrypt.hash(password, saltRounds);
+};
+
+// returns promise!
+const comparePassword = (plainPassword, hashedPassword) => {
+    console.log("plain: ", plainPassword)
+    console.log("hash: ", hashedPassword)
+    return bcrypt.compare(plainPassword, hashedPassword);
+};
+
+module.exports = { hashPassword, comparePassword };
