2a34686ff38b41c75c1e0a859413b998b72e40f3
Max Kroslak <max.kroslak@gmai;.com>
2025-04-19 18:03:52 +0200

    creating session and adding routes

--- my-app-be/app.js
+++ my-app-be/app.js
@@ -1,34 +1,15 @@
-/*
-const express = require('express');
-const cors = require('cors');
-const pool = require('./db');
-require('dotenv').config();
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(express.json());
-
-// Example route to get data
-app.get('/api/', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM users');
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
-
-
-module.exports = app;
-*/
-
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+var session = require('express-session');
+const PgSession = require("connect-pg-simple")(session);
+var pool = require('./config/db.js');
+
+const { config } = require('./config/config.js');
+require('dotenv').config()
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -40,6 +21,8 @@ var addUserRouter = require('./routes/api_v1/users');
 
 var reviewByRestaurantId = require('./routes/api_v1/reviews');
 //var restaurantReviewAvg = require('./routes/api_v1/reviews');
+
+var authRouter = require('./routes/api_v1/auth');
 
 var app = express();
 
@@ -55,6 +38,35 @@ app.use(cookieParser());
 // serves static files
 // app.use(express.static(path.join(__dirname, 'public')));
 
+
+if (process.env.STATUS === 'production') {
+    // trust proxy needed for secure cookie to work on render.com
+    // because render.com uses a reverse proxy to handle HTTPS requests
+    // and forwards the requests to the backend server over HTTP
+    app.set('trust proxy', 1);
+}
+
+
+app.use(
+    session({
+        store: new PgSession({
+            pool, 
+            tableName: "session", 
+        }),        
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        name: config.session.cookieName,
+        cookie: {
+            secure: process.env.STATUS === 'production',
+            httpOnly: true,
+            sameSite: process.env.STATUS === 'production'?'none':'lax',
+            maxAge: 1000 * 60 * 60 * 24 }            
+             
+    })    
+);
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1/restaurants', restaurantsRouter);
@@ -64,5 +76,7 @@ app.use('/api/v1/users', addUserRouter);
 
 app.use('/api/v1/reviews/', reviewByRestaurantId);
 //app.use('/api/v1/reviews/', restaurantByIdRouter);
+
+app.use('/api/v1/auth', authRouter);
 
 module.exports = app;
