c1f5cdfc3ba3d36254e2d19ca9a1325863207f81
Max Kroslak <max.kroslak@gmai;.com>
2025-04-19 18:01:22 +0200

    relocating search from main to Restaurants page

--- my-app-fe/src/App.tsx
+++ my-app-fe/src/App.tsx
@@ -10,47 +10,24 @@ import Account from './pages/account';
 import { Navigate } from 'react-router-dom';
 
 import { useEffect, useState } from 'react';
-import { getRestaurant } from './services/restaurantService'
+import { getRestaurant } from './services/restaurantService';
 import { Restaurant } from './types';
+
 
 function App() {
 
-  const [restaurants, setRestaurant] = useState<Restaurant[]>([]);
-  const [search, setSearch] = useState<string>('') 
-  console.log(search) 
-
-  // periodically refresh (timer)
-  useEffect(() => {
-    getRestaurant().then(
-      (restaurants) => setRestaurant(restaurants)
-    );
-
-    const fetchMessagesInterval = setInterval(() => {
-        getRestaurant().then(
-          (restaurant) => setRestaurant(restaurant)
-        );
-      }, 10000);
-    return () => clearInterval(fetchMessagesInterval);
-  }, []);
+  const [error, setError] = useState('');
+  const [authStatus, setAuthStatus] = useState(false);
 
   return (
     <Router>
       <NavBar />
-      <div className='search'>
-        <form className="flex gap-2">
-          <input
-            type="text"
-            placeholder="Zadajte reštauráciu..."
-            onChange={(e) => setSearch(e.target.value)}
-          />
-        </form>
-      </div>
       <Routes>
         <Route path="/" element={<Navigate to="/restaurants" />} />
-        <Route path="/restaurants/" element={<RestaurantListPage restaurants={restaurants} search={search}/>} />
+        <Route path="/restaurants/" element={<RestaurantListPage />} />
         <Route path="/account" element={<Account />} />
         <Route path="/signUp" element={<SignUp />} />
-        <Route path="/logIn" element={<LogIn />} />
+        <Route path="/logIn" element={<LogIn error={error} setError={setError} setAuthStatus={setAuthStatus} />} />
         <Route path="/search" element={<Search />} />
         <Route path="/restaurants/:id" element={<RestaurantPage />} />
       </Routes>
