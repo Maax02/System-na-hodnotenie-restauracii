d9e5ceefd92e73c04793146afe6f3ad15d660e7c
Max Kroslak <max.kroslak@gmai;.com>
2025-04-19 18:02:02 +0200

    adding dynamic search for restaurant

--- my-app-fe/src/pages/restaurantListPage.tsx
+++ my-app-fe/src/pages/restaurantListPage.tsx
@@ -2,14 +2,44 @@ import RestaurantCardDB from "../components/RestaurantCardDB";
 import { Restaurant } from '../types';
 //import { useEffect, useState } from 'react';
 
-interface Props {
-    restaurants: Restaurant[];
-    search: string
-}
+import { useEffect, useState } from 'react';
+import { getRestaurant } from '../services/restaurantService';
 
-function RestaurantListPage( { restaurants, search }: Props) {
-    console.log("RestaurantListPage search:", search);
-    return <RestaurantCardDB restaurants={restaurants} search={search} />;
+
+function RestaurantListPage() {
+    const [restaurants, setRestaurant] = useState<Restaurant[]>([]);
+    const [search, setSearch] = useState<string>('') 
+    console.log(search) 
+  
+    // periodically refresh (timer)
+    useEffect(() => {
+      getRestaurant().then(
+        (restaurants) => setRestaurant(restaurants)
+      );
+  
+      const fetchMessagesInterval = setInterval(() => {
+          getRestaurant().then(
+            (restaurant) => setRestaurant(restaurant)
+          );
+        }, 10000);
+      return () => clearInterval(fetchMessagesInterval);
+    }, []);
+
+
+    return (
+        <>
+            <div className='search'>
+                <form className="flex gap-2">
+                <input
+                    type="text"
+                    placeholder="Zadajte reštauráciu..."
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+                </form>
+            </div>
+            <RestaurantCardDB restaurants={restaurants} search={search} />
+        </>
+    )
   }
   
   export default RestaurantListPage;
