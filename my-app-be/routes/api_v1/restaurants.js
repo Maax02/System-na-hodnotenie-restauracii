56e5da3c0b46de0ef9c1fb0c65c789b857a7a36b
Max Kroslak <max.kroslak@gmai;.com>
2025-04-20 16:41:53 +0200

    getting user reviews

--- my-app-be/routes/api_v1/restaurants.js
+++ my-app-be/routes/api_v1/restaurants.js
@@ -1,5 +1,6 @@
 var express = require('express'); // ESM: import
-var { getRestaurants, getRestaurantById } = require('../../models/restaurants')
+var { getRestaurants, getRestaurantById } = require('../../models/restaurants');
+const { getUserReviews } = require('../../models/reviews');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
@@ -29,4 +30,18 @@ router.get('/:id', function (req, res, next) {
     );
 });
 
+
+router.get('/user/:id', function (req, res, next) {
+    const id = req.params.id;
+    getUserReviews(id).then(
+        (restaurant) => {
+            res.json(restaurant.rows);
+        }
+    ).catch(
+        (err) => {
+            console.log(err);
+            res.status(500).send("Error fetching user reviews by user id");
+        }
+    );
+});
 module.exports = router; // ESM: export
