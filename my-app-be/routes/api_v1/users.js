6add8df35ae9685ca52deb484440b321e8bc93a6
Max Kroslak <max.kroslak@gmai;.com>
2025-04-22 16:38:52 +0200

    adding registration message

--- my-app-be/routes/api_v1/users.js
+++ my-app-be/routes/api_v1/users.js
@@ -4,11 +4,11 @@ var router = express.Router();
 
 router.post('/', function (req, res, next) {
     addUser(req.body).then(
-        (r) => res.status(200)
+        (r) => res.status(201).json({ message: 'Registrácia prebehla úspešne!' })
     ).catch(
         (e) => {
             console.log(e);
-            res.status(500);
+            res.status(500).json({ error: 'Registrácia neúspešná. Meno alebo email sa už používa.' });
         }
     );
 });
--- my-app-fe/src/pages/signUp.tsx
+++ my-app-fe/src/pages/signUp.tsx
@@ -1,49 +1,77 @@
 import { useState } from 'react';
-import '/src/css/signUp.css'
-import { addUser } from '../services/signUpService'
+import '/src/css/signUp.css';
+import { addUser } from '../services/signUpService';
 
 function SignUp() {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [message, setMessage] = useState<string | null>(null);
 
-    function signUp() {
-        addUser({
-            'user_name': username,
-            'user_password': password,
-            'user_email': email
-        });
+    async function signUp() {
+        try {
+            const response = await addUser({
+                user_name: username,
+                user_password: password,
+                user_email: email
+            });
 
-        setUsername('');
-        setEmail('');
-        setPassword('');
+            const data = await response.json();
+
+            if (response.ok) {
+                setMessage(data.message);
+                setUsername('');
+                setEmail('');
+                setPassword('');
+            } else {
+                setMessage(data.error || 'Registrácia neúspešná.');
+            }
+        } catch (err) {
+            console.error('Sign up error:', err);
+            setMessage('Nastala nečakaná chyba.');
+        }
     }
 
     return (
         <div className="signLog">
             <div className="header">
-                <div className="text"> Sign up </div>
+                <div className="text">Sign up</div>
                 <div className="underline"></div>
             </div>
             <div className="inputs">
                 <div className="input">
-                    <input type="name" placeholder='Name'
-                    value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
                 </div>
                 <div className="input">
-                    <input type="email" placeholder='Email'
-                    value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
                 <div className="input">
-                    <input type="password" placeholder='Password'
-                    value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
             </div>
             <div className="submits">
-                <div className="submit" onClick={signUp}> Sign Up </div>
+                <div className="submit" onClick={signUp}>
+                    Sign Up
+                </div>
             </div>
+            {message && <p className="signup-message">{message}</p>}
         </div>
-    )
+    );
 }
 
 export default SignUp;
